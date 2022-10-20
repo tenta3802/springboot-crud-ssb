df0481ad98192e70b1a3423f95336217e3e60fbb
tenta <eunoia3802@gmail.com>
2022-10-20 23:22:58 +0900

    spring boot crud 구현 연습 day03

---
+++ src/main/java/com/site/ssb/controller/QuestionController.java
@@ -0,0 +1,24 @@
+package com.site.ssb.controller;
+
+import com.site.ssb.Repository.QuestionRepository;
+import com.site.ssb.entity.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+    private final QuestionRepository questionRepository;
+
+
+    @GetMapping(value = "/question/list")
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+}
